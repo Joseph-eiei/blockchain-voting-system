40339bc48a0ddb9adb27a3fdf125d4f2ec3e6509
Kj <87913316+ommie123@users.noreply.github.com>
2025-05-27 20:19:12 +0700

    Update config.js

--- frontend/src/contracts/config.js
+++ frontend/src/contracts/config.js
@@ -1,15 +1,14 @@
-require("dotenv").config();
 import EM from "./ElectionManager.json";
 import CM from "./CandidateManager.json";
 import BQ from "./Ballot.json";
 import RS from "./Results.json";
 import VR from "./VoterRegistry.json";
 
-export const VR_ADDRESS = process.env.VR_ADDRESS;
-export const EM_ADDRESS = process.env.EM_ADDRESS;
-export const CM_ADDRESS = process.env.CM_ADDRESS;
-export const BQ_ADDRESS = process.env.BQ_ADDRESS;
-export const RS_ADDRESS = process.env.RS_ADDRESS;
+export const VR_ADDRESS = import.meta.env.VITE_VR_ADDRESS;
+export const EM_ADDRESS = import.meta.env.VITE_EM_ADDRESS;
+export const CM_ADDRESS = import.meta.env.VITE_CM_ADDRESS;
+export const BQ_ADDRESS = import.meta.env.VITE_BQ_ADDRESS;
+export const RS_ADDRESS = import.meta.env.VITE_RS_ADDRESS;
 
 export const EM_ABI = EM.abi;
 export const CM_ABI = CM.abi;
